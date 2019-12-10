0231998998101f42fd5c6b7cb683220b34b7cc3f
Jason Kiraly <jasonkiraly@MacBook-Pro.hitronhub.home>
2019-12-10 14:48:04 -0800

    Added stubbed in users data.

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require("graphql");
-
+const _ = require("lodash");
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -11,13 +11,28 @@ const UserType = new GraphQLObjectType({
   }
 });
 
+const users = [
+  {
+    id: "23",
+    firstName: "Bill",
+    age: 20
+  },
+  {
+    id: "47",
+    firstName: "Sammy",
+    age: 21
+  }
+];
+
 const rootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {}
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
     }
   }
 });
