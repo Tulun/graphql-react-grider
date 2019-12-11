5e3c990c0e7c31f7073c0c491b95093717e22288
Jason Kiraly <jasonkiraly@MacBook-Pro.hitronhub.home>
2019-12-10 16:41:34 -0800

    resolved circular reference issue in graphQL.

--- schema/schema.js
+++ schema/schema.js
@@ -1,32 +1,45 @@
 const graphql = require("graphql");
 const axios = require("axios");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(pv, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${pv.id}/users`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       resolve(pv, args) {
-        // console.log(pv, args);
         return axios
           .get(`http://localhost:3000/companies/${pv.companyId}`)
           .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
