69360192d94222a636324349c6d91fa0ac18e436
Jason Kiraly <jasonkiraly@Jasons-MacBook-Pro.local>
2019-12-13 15:59:55 -0800

    Song create.

--- lyrical/client/components/SongList.js
+++ lyrical/client/components/SongList.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
-class SongList extends Component {
-  render() {
-    if (this.props.data.loading) return <div>Loading...</div>;
-    return (
-      <ul className="collection">
-        {this.props.data.songs.map(({ title, id }) => {
-          return (
-            <li key={id} className="collection-item">
-              {title}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+const SongList = ({ data }) => {
+  if (data.loading) return <div>Loading...</div>;
+  return (
+    <ul className="collection">
+      {data.songs.map(({ title, id }) => {
+        return (
+          <li key={id} className="collection-item">
+            {title}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const query = gql`
   {
