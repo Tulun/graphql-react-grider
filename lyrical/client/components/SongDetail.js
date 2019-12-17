298dbfec647c6fc072d1c2f07b0ec2b713d98a3c
Jason Kiraly <jasonkiraly@MacBook-Pro.hitronhub.home>
2019-12-17 14:31:04 -0800

    auto fetch the song data from graphql.

--- lyrical/client/components/SongDetail.js
+++ lyrical/client/components/SongDetail.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import fetchSong from "../queries/fetchSong";
 
-const SongDetail = () => {
+const SongDetail = props => {
+  console.log(props);
   return (
     <div>
       <h3>SongDetail</h3>
@@ -8,4 +11,6 @@ const SongDetail = () => {
   );
 };
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: props => ({ variables: { id: props.params.id } })
+})(SongDetail);
