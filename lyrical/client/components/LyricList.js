595e84582bab2ab984a9bac212cdc3ce79b2602d
Jason Kiraly <jasonkiraly@MacBook-Pro.hitronhub.home>
2019-12-17 17:03:00 -0800

    fixed onclick handler for like.

--- lyrical/client/components/LyricList.js
+++ lyrical/client/components/LyricList.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+
   render() {
     if (!this.props.lyrics.length) return <div></div>;
     return (
       <ul className="collection">
-        {this.props.lyrics.map(({ content }, id) => (
-          <li className="collection-item" key={id}>
+        {this.props.lyrics.map(({ content, id }, index) => (
+          <li className="collection-item" key={index}>
             {content}
+            <i className="material-icons" onClick={() => this.onLike(id)}>
+              thumb_up
+            </i>
           </li>
         ))}
       </ul>
